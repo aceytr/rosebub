ee8acdf824f712d30f4846c215594a1aaae7fb43
rosebub <admin@rosebub.com>
2021-09-05 16:23:23 +0200

    Deleted Mycalendar Link

--- www/layouts/menu.php
+++ www/layouts/menu.php
@@ -7,7 +7,6 @@
 	<?php if( !empty($_SESSION['user_id']) ): ?>
 		<a href="<?= helperUrl_link('home','index'); ?>"><?= $app_lang['mod_home']; ?></a> - 
 		<a href="<?= helperUrl_link('myprofil','index'); ?>"><?= $app_lang['mod_myprofil']; ?></a> - 
-		<a href="<?= helperUrl_link('mycalendars','index'); ?>"><?= $app_lang['mod_mycalendars']; ?></a> - 
 		<a href="<?= helperUrl_link('auth','logout'); ?>"><?= $app_lang['mod_auth_signout']; ?></a>
 	<?php endif; ?>
 
