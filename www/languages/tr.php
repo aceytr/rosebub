8eb35ac47cf205a66cdb386e6e572a3f553f5d16
Abdullah Ceylan <info@abdullahceylan.com.tr>
2021-09-09 23:23:15 +0300

    Create tr.php
    Turkish language

---
+++ www/languages/tr.php
@@ -0,0 +1,9 @@
+<?php
+
+$app_lang = array();
+
+$app_lang['mod_home'] = 'Anasayfa';
+$app_lang['mod_auth_login'] = 'Giriş';
+$app_lang['mod_auth_signout'] = 'Çıkış Yap';
+$app_lang['mod_myprofil'] = 'Profilim';
+$app_lang['mod_mycalendars'] = 'Takvimim';
