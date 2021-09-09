e831ce1bfc48cd04454404e6b2384c545f1bab20
Abdullah Ceylan <info@abdullahceylan.com.tr>
2021-09-09 23:27:25 +0300

    Create tr.php
    add Turkish language

---
+++ www/modules/auth/languages/tr.php
@@ -0,0 +1,16 @@
+<?php
+
+$mod_lang  = array();
+
+$mod_lang['title'] = 'Yetkilendirme';
+$mod_lang['email'] = 'E-Posta';
+$mod_lang['login_submit'] = 'Giriş Yap';
+$mod_lang['login_false'] = 'Geçersiz Oturum!';
+$mod_lang['password_forgot_question'] = 'Şifrenimi Unuttun ?';
+$mod_lang['password'] = 'Şifre';
+$mod_lang['password_new'] = 'Yeni Şifre';
+$mod_lang['password_forgot_email_subject'] = 'Yeni Şifreniz';
+$mod_lang['password_forgot_email_title'] = 'Şifre Sıfırlama İsteğiniz';
+$mod_lang['password_forgot_email_description'] = 'Size eski şifrenizi öylece gönderemeyiz. Parolanızı sıfırlamak için benzersiz bir bağlantı sizin için oluşturuldu. Parolanızı sıfırlamak için aşağıdaki bağlantıya tıklayın ve talimatları izleyin.';
+$mod_lang['password_forgot_email_link_text'] = 'Şifrenizi Sıfırlayın';
+$mod_lang['password_new_send'] = 'Yeni Şifreniz E-Posta İle Gönderildi';
