186a69b1293f8f904bcb43036093bcd473136e7c
Abdullah Ceylan <info@abdullahceylan.com.tr>
2021-09-09 23:30:04 +0300

    Create tr.php
    add Turkish language

---
+++ www/modules/myprofil/languages/tr.php
@@ -0,0 +1,18 @@
+<?php
+
+$mod_lang = array();
+
+$mod_lang['title'] = 'Profilim';
+$mod_lang['profil_update'] = 'Güncelle';
+
+$mod_lang['first_name'] = 'Ad';
+$mod_lang['last_name'] = 'Soyad';
+$mod_lang['email'] = 'E-Posta';
+$mod_lang['phone_mobile'] = 'Telefon Numarası';
+$mod_lang['address_street'] = 'Sokak/Cadde';
+$mod_lang['address_postalcode'] = 'Posta Kodu';
+$mod_lang['address_city'] = 'Şehir';
+$mod_lang['address_country'] = 'Ülke';
+$mod_lang['birthdate'] = 'Doğum Tarihi';
+$mod_lang['lang'] = 'Dil Seçin';
+$mod_lang['save_ok'] = 'Profiliniz Başarıyla Güncellendi !';
